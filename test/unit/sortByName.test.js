7db6482e6a68bcc2043fe61d7759131d545fb21f
Akimutina <120643609+Akimutina@users.noreply.github.com>
2023-07-29 20:53:53 +0700

    first commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,23 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("There is no sorting of book titles of the same length", () => {
+    expect(
+      sorting.sortByName(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"])
+    ).toEqual(["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"]);
+  });
+});
